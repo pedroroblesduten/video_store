5a100ee548c6fa3dd70bdb2dcb56485c5f4fa967
Pedro Robles <96757067+pedroroblesduten@users.noreply.github.com>
2022-12-05 02:40:08 +0000

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -1,23 +1,37 @@
 import java.util.Enumeration;
 
 public class HtmlStatement extends Statement {
+  
+  private String getResultText(Customer aCustomer) {
+    return "<H1>Rentals for <EM>" + aCustomer.getName() +
+        "</EM></H1><P>\n";
+  }
+
+  private String getResultMovie(Rental rental) {
+    return rental.getMovie().getTitle() + ": " +
+        String.valueOf(rental.getCharge()) + "<BR>\n";
+  }
+
+  private String addFooter(Customer aCustomer) {
+    String result = "";
+    result += "<P>You owe <EM>" +
+        String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
+    result += "On this rental you earned <EM>" +
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        "</EM> frequent renter points<P>";
+    return result;
+  }
 
  public String value(Customer aCustomer) {
     Enumeration rentals = aCustomer.getRentals();
-    String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-    "</EM></H1><P>\n";
+    String result = getResultText(aCustomer);
     while (rentals.hasMoreElements()) {
        Rental each = (Rental) rentals.nextElement();
        //show figures for each rental
-       result += each.getMovie().getTitle()+ ": " +
-       String.valueOf(each.getCharge()) + "<BR>\n";
+       result += getResultMovie(each);
     }
     //add footer lines
-    result += "<P>You owe <EM>" +
-    String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-    result += "On this rental you earned <EM>" + 
-    String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-    "</EM> frequent renter points<P>";
+    result += addFooter(aCustomer);
     return result;
  }
 
--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,36 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
-   public String value(Customer aCustomer) {
+  
+  private String getResultText(Customer aCustomer) {
+    return "Rental Record for " + aCustomer.getName() + "\n";
+  }
+
+  private String getResultMovie(Rental rental) {
+    
+    return "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
+  }
+
+  private String addFooter(Customer aCustomer) {
+    
+    String result = "";
+    result += "Amount owed is " +
+        String.valueOf(aCustomer.getTotalCharge()) + "\n";
+    result += "You earned " +
+        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+        " frequent renter points";
+    return result;
+  }
+ public String value(Customer aCustomer) {
     Enumeration rentals = aCustomer.getRentals();
-    String result = "Rental Record for " + aCustomer.getName() +
-    "\n";
+    String result = getResultText(aCustomer);
     while (rentals.hasMoreElements()) {
        Rental each = (Rental) rentals.nextElement();
-       //show figures for this rental
-       result += "\t" + each.getMovie().getTitle()+ "\t" +
-       String.valueOf(each.getCharge()) + "\n";
+       //show figures for each rental
+       result += getResultMovie(each);
     }
     //add footer lines
-    result += "Amount owed is " +
-    String.valueOf(aCustomer.getTotalCharge()) + "\n";
-    result += "You earned " +
-    String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-    " frequent renter points";
+    result += addFooter(aCustomer);
     return result;
  }
 
